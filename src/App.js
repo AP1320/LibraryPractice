1ac2e5e3452abc3bc12cc76c1bcb2286a93d1a8d
AP1320 <127207618+AP1320@users.noreply.github.com>
2023-03-08 22:10:20 +0300

    Add files via upload
    Signed-off-by: AP1320 <127207618+AP1320@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,34 @@ function App() {
 
             <div className="App">
                 <h1 className="hello">Поиск книг</h1>
+                <form>
+                    <label>
+                        <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Отправить" />
+
+                </form>
+
+                <form>
+                    <select>Категории:
+                        <option value="all">all</option>
+                        <option value="ART">ART</option>
+                        <option selected value="Computers">Computers</option>
+                       
+                    </select>
+
+                    <select>Сортировка по:
+                        <option value="revelance">revelance</option>
+
+                    </select>
+
+
+                </form>
+
+
+
             </div>
+
 
 
     );
