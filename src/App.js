b4f049cb180402685f18f7de369a19a1e9b3fe04
AP1320 <127207618+AP1320@users.noreply.github.com>
2023-03-15 23:37:45 +0300

    Add files via upload
    Signed-off-by: AP1320 <127207618+AP1320@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from "./Header";
-import Books from "./Books";
+import Books from  "./Books";
 import React from "react";
 
 function App() {
--- src/Books.js
+++ src/Books.js
@@ -1,7 +1,17 @@
 import './App.css';
 import Header from "./Header";
+import React from "react";
 
 function Books() {
+    const apiGet=()=>
+    {
+        fetch('https://api.npms.io/v2/search?q=react')
+            .then(response => response.json())
+            .then(data => this.setState({ totalReactPackages: data.total }));
+    }
+
+
+
 
 
 
@@ -10,7 +20,9 @@ function Books() {
 
 
         <div className="Books">
-            
+            MY API<br/>
+            <button onClick={apiGet}><Books/></button>
+
         </div>
 
 
