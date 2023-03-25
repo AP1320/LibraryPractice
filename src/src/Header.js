cf430910a5b2a995ef46b625b23765cf08939acf
AP1320 <127207618+AP1320@users.noreply.github.com>
2023-03-26 00:16:18 +0300

    Add files via upload
    Signed-off-by: AP1320 <127207618+AP1320@users.noreply.github.com>

---
+++ src/src/Header.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function Header() {
+
+
+    return (
+
+
+            <div className="Header">
+                <h1>Поиск книг</h1>
+                <form action="">
+                    <input type="text"/>
+                    <button type="submit">Поиск</button>
+                </form>
+            </div>
+
+
+
+
+
+    );
+}
+
+export default Header;
